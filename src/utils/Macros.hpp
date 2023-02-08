c53aee58fbf2e58fdb349a52c5fe1e9336bad1e2
Luohao Wang <Luohaothu@users.noreply.github.com>
2023-02-08 22:45:44 +0800

    [misc] Wrap macros with do-while

--- src/utils/Macros.hpp
+++ src/utils/Macros.hpp
@@ -115,23 +115,31 @@ namespace OpFlow {
 
 #ifndef NDEBUG
 #define OP_ASSERT(X)                                                                                         \
-    if (!(X)) {                                                                                              \
-        OP_ERROR("Assertion {} failed.", #X);                                                                \
-        OP_ABORT;                                                                                            \
-    }
+    do {                                                                                                     \
+        if (!(X)) {                                                                                          \
+            OP_ERROR("Assertion {} failed.", #X);                                                            \
+            OP_ABORT;                                                                                        \
+        }                                                                                                    \
+    } while (0)
 #define OP_ASSERT_MSG(X, ...)                                                                                \
-    if (!(X)) {                                                                                              \
-        OP_ERROR("Assertion {} failed.", #X);                                                                \
-        OP_ERROR(__VA_ARGS__);                                                                               \
-        OP_ABORT;                                                                                            \
-    }
+    do {                                                                                                     \
+        if (!(X)) {                                                                                          \
+            OP_ERROR("Assertion {} failed.", #X);                                                            \
+            OP_ERROR(__VA_ARGS__);                                                                           \
+            OP_ABORT;                                                                                        \
+        }                                                                                                    \
+    } while (0)
 #define OP_EXPECT(X)                                                                                         \
-    if (!(X)) { OP_WARN("Expectation {} failed.", #X); }
+    do {                                                                                                     \
+        if (!(X)) { OP_WARN("Expectation {} failed.", #X); }                                                 \
+    } while (0)
 #define OP_EXPECT_MSG(X, ...)                                                                                \
-    if (!(X)) {                                                                                              \
-        OP_WARN("Expectation {} failed.", #X);                                                               \
-        OP_WARN(__VA_ARGS__);                                                                                \
-    }
+    do {                                                                                                     \
+        if (!(X)) {                                                                                          \
+            OP_WARN("Expectation {} failed.", #X);                                                           \
+            OP_WARN(__VA_ARGS__);                                                                            \
+        }                                                                                                    \
+    } while (0)
 #else
 #define OP_ASSERT(X)
 #define OP_EXPECT(X)
